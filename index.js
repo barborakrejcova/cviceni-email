223ecc99722b61cc62153a1863b7290d947f2081
Barbora Krejčová <krejcova.barbora96@gmail.com>
2024-11-29 14:34:47 +0100

    cviceni email

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+//Cviceni 1 + 2 - E-mail, pozdravy + telo
 
+const goodbye = jmeno => {
+    return 'S pozdravem ' + jmeno;
+}
+
+const fillSubject = subject => {
+    const subjectEl = document.querySelector('.email__subject');
+    subjectEl.textContent = `${subject}`;
+}
+
+fillSubject('Motivační dopis');
+
+const fillBody = (body, name) => {
+    const bodyEl = document.querySelector('.email__body');
+    bodyEl.innerHTML += body;
+    const closingEl = document.querySelector('.email__closing');
+    closingEl.textContent = goodbye(name);
+}
+
+fillBody('Dobrý den, blablabla', 'Barbora Krejčová');
